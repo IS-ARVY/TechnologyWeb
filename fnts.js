8da00023b01f766308840c2473a0c8032f882695
IS-ARVY <84204170+IS-ARVY@users.noreply.github.com>
2021-05-14 13:50:19 -0600

    Add files via upload

---
+++ fnts.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
+
+  
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+  instance.open();
+
+  
+instance.destroy();
+
+
+$(document).ready(function(){
+
+	$('.ir-arriba').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 300);
+	});
+
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+			$('.ir-arriba').slideDown(300);
+		} else {
+			$('.ir-arriba').slideUp(300);
+		}
+	});
+
+});
